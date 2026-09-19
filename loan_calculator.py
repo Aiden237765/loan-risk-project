5510c59fd844dff2a9ad6740183efbf0fd1d651a
Aiden <aiden.pink@ndus.edu>
2026-09-19 10:59:14 -0500

    Create loan payment calculator

---
+++ loan_calculator.py
@@ -0,0 +1,14 @@
+﻿loan_amount = 20000
+annual_rate = 0.06
+years = 5
+
+monthly_rate = annual_rate / 12
+number_of_payments = years * 12
+
+monthly_payment = (
+    loan_amount * monthly_rate
+    / (1 - (1 + monthly_rate) ** (-number_of_payments))
+)
+
+print("Loan Amount: $", loan_amount)
+print("Monthly Payment: $", round(monthly_payment, 2))
